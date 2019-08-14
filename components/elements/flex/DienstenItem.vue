906979fb81fbf1c16159bd4f9a7a3a329f3a22a4
Mitchel van Eijgen <mvaneijgen@gmail.com>
2019-08-14 13:42:59 +0200

    Fixed double <p> error

--- components/elements/flex/DienstenItem.vue
+++ components/elements/flex/DienstenItem.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="elements-DienstenItem alloy-cards alloy-cards--diensten">
+
     <div class="container-col">
       <span class="spacer col-60"></span>
       <h2 class="alloy-title col-40" v-html="item.title"></h2>
     </div>
+
     <div class="inner">
       <div class="container-col">
+
         <div class="alloy-content col-40">
-          <p v-html="item.excerpt"></p>
+          <span v-html="item.excerpt"></span>
           <div class="alloy-btn-group">
             <nuxt-link :to="item.slug" class="btn">Lees meer</nuxt-link>
           </div>
         </div>
+
         <div class="alloy-illustration col-60">
           <component :is="componentSVG"></component>
         </div>
       </div>
     </div>
+
   </div>
 </template>
 

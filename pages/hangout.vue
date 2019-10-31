de443d6ca96a8934efad4455966a228d8936aeb2
Mitchel van Eijgen <mvaneijgen@gmail.com>
2019-10-31 08:20:24 +0100

    Add hangout page

---
+++ pages/hangout.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <h1>Hangout</h1>
+    <p>Redirect to <a :href="this.url">{{this.url}}</a></p>
+    <a :href="this.url" class="btn">Open Hangout</a>
+  </main>
+</template>
+
+<script>
+export default {
+  title: "Hangout",
+  data() {
+    return {
+      url: "https://hangouts.google.com/call/WqrqlgDA8vfQV1_NQ7XFAEEI",
+    };
+  },
+  created() {
+    if (process.client) {
+      window.location.replace(this.url);
+    }
+  },
+};
+</script>
